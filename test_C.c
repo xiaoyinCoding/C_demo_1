6c02af787b7a9c78f1845f11f45e5688deaf058c
weila <604555185@qq.com>
2022-10-02 04:25:09 +0800

    动态内存分配

--- test_C.c
+++ test_C.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <malloc.h>
+
+void f(int * q)
+{
+	*q = 200;
+}
 
 int main(void)
 {
-	int i = 6;
-	int* p;
-	p = &i;
-	printf("i = %d\n", i);
-	printf("pµÄµØÖ·Îª£º %#x", p);
+	int* p = (int*)malloc(sizeof(int));
+	*p = 10;
+
+	printf("%d\n", *p);
+
+	f(p);
+
+	printf("%d\n", *p);
+
+	return 0;
 }
